bd85fa1e52364978d90612d370771ff2bc45d81d
Hamed Noroozi <noruzi206@gmail.com>
2024-08-04 23:09:21 +0330

    update form of send otp

--- Frontend/src/Pages/AuthPage.jsx
+++ Frontend/src/Pages/AuthPage.jsx
@@ -3,14 +3,14 @@ import SendOtpForm from "../components/templates/SendOtpForm";
 import CheckOtpFrom from "../components/templates/CheckOtpFrom";
 
 function AuthPage(props) {
-   const [step, useStep] = useState(1)
-   const [mobile, useMobile] = useState("")
-   const [code, useCode] = useState("")
+   const [step, setStep] = useState(1)
+   const [mobile, setMobile] = useState("")
+   const [code, setCode] = useState("")
 
 
    return (
       <div>
-         {step === 1 && <SendOtpForm />}
+         {step === 1 && <SendOtpForm setStep={setStep} setMobile={setMobile} mobile={mobile} />}
          {step === 2 && <CheckOtpFrom />}
       </div>
    );
