003adcdf4665d54c9294ba16801f47d1c3e6f994
Hamed Noroozi <noruzi206@gmail.com>
2024-08-11 22:06:27 +0330

    install tailwind css and add basic styles

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-
-import "styles/index.css";
-import "styles/fonts.css";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      fontFamily:{
+       "vazir-Thin": "Vazir Thin",
+       "vazir-Regular": "Vazir Regular",
+       "vazir-Medium": "Vazir Medium",
+       "vazir-Bold": "Vazir Bold",
+       "vazir-ExtraBold": "Vazir ExtraBold",
+      },
+      extend: {},
+   },
+   plugins: [],
+}
+
