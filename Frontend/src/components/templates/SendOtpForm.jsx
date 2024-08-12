c39bd46d01d019dd0b969f04699d5683c88bcb94
Hamed Noroozi <noruzi206@gmail.com>
2024-08-12 20:53:47 +0330

    add styles of send otp form component

--- Frontend/src/components/templates/SendOtpForm.jsx
+++ Frontend/src/components/templates/SendOtpForm.jsx
@@ -14,9 +14,12 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
       console.log({ response, error });
    }
    return (
-      <form onSubmit={submitHandler}>
-         <p>ورود به حساب کاربری</p>
-         <span>
+      <form 
+      onSubmit={submitHandler}
+      className='bg-white max-w-[500px] mx-3 flex flex-col mt-24 shadow-md rounded p-8 shadow-topLeft
+      sm:mx-auto'>
+         <p className='text-lg font-normal mb-4 pb-2 border-b-2'>ورود به حساب کاربری</p>
+         <span className='text-slate-500 text-sm mb-4'>
             برای استفاده از امکانات دیوار، لطفا شماره موبایل خود را وارد کنید. کد تایید به این شماره پیامک خواهد شد.
          </span>
          <label htmlFor="input" >شماره موبایل خود را وارد کنید</label>
@@ -26,8 +29,10 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
             placeholder='شماره موبایل'
             value={mobile}
             onChange={e => setMobile(e.target.value)}
-         />
-         <button type='submit' >ارسال کد تایید</button>
+            className='mt-2.5 mb-5 p-1.5 outline-none'/>
+         <button 
+         className='w-28 py-1 px-2.5 hover:bg-primary-Hover'
+         type='submit'>ارسال کد تایید</button>
       </form>
    );
 }
