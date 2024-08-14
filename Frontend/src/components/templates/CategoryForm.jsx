83008ceecf7437d951d94ad00434a4d29348c092
Hamed Noroozi <noruzi206@gmail.com>
2024-08-14 22:30:15 +0330

    show message for 3s and clear inputs

--- Frontend/src/components/templates/CategoryForm.jsx
+++ Frontend/src/components/templates/CategoryForm.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { addCategory } from 'services/admin';
 
 function CategoryForm() {
    const queryClient = useQueryClient();
+   const [showMessage, setShowMessage] = useState(false);
    const [form, setForm] = useState({ name: "", slug: "", icon: "" })
 
    const { mutate, isLoading, error, data } = useMutation(addCategory, {
@@ -20,7 +21,19 @@ function CategoryForm() {
       if (!form.name || !form.slug || !form.icon) return;
 
       mutate(form);
+
+      for (let i = 0; i < 3; i++) {
+         event.target[i].value = "";
+      }
+      setShowMessage(true);
    }
+
+   useEffect(()=>{
+      setTimeout(() => {
+         setShowMessage(false);
+      }, 3000);
+   },[showMessage]);
+
    return (
       <form
          onChange={changeHandler}
@@ -31,8 +44,8 @@ function CategoryForm() {
 
          <h3 className='mb-8 border-b-2 border-primary w-fit pb-1 font-semibold'>دسته بندی جدید</h3>
 
-         {!!error && <p className='bg-primary'>مشکلی پیش آمده است، لطفا دوباره امتحان کنید!</p>}
-         {data?.status === 201 && <p className='bg-lime-700'>دسته بندی با موفقیت اضافه شد</p>}
+         {!!error && showMessage && <p className='bg-primary'>مشکلی پیش آمده است، لطفا دوباره امتحان کنید!</p>}
+         {data?.status === 201 && showMessage && <p className='bg-lime-700'>دسته بندی با موفقیت اضافه شد</p>}
 
          <label htmlFor="name">اسم دسته بندی</label>
          <input type="text" name='name' id='name' />
