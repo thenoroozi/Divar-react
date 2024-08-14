293cae7027fd11756f2df6876b19c872395e29c5
Hamed Noroozi <noruzi206@gmail.com>
2024-08-15 00:25:34 +0330

    add styles of addPost

--- Frontend/src/components/templates/AddPost.jsx
+++ Frontend/src/components/templates/AddPost.jsx
@@ -26,11 +26,18 @@ const AddPost = () => {
    const submitHandler = (event) => {
       event.preventDefault();
       console.log(form);
-      
+
    }
    return (
-      <form onSubmit={submitHandler} onChange={changeHandler}>
-         <h3>افزودن آگهی</h3>
+      <form
+         onSubmit={submitHandler}
+         onChange={changeHandler}
+         className='[&_label]:block [&_label]:text-sm [&_label]:mb-2
+            [&_input]:block [&_input]:w-72 [&_input]:p-1 [&_input]:mb-5
+            [&_textarea]:block [&_textarea]:w-72 [&_textarea]:h-24 [&_textarea]:mb-5 [&_textarea]:border [&_textarea]:border-zinc-400 [&_textarea]:rounded [&_textarea]:p-1
+            [&_select]:w-72 [&_select]:block [&_select]:bg-white [&_select]:border [&_select]:border-zinc-400 [&_select]:rounded [&_select]:mb-5 [&_select]:p-1'>
+
+         <h3 className='mb-8 border-b-2 border-primary w-fit pb-1 font-semibold'>افزودن آگهی</h3>
          <label htmlFor="title">عنوان</label>
          <input type="text" name="title" id="title" />
          <label htmlFor="content">توضیحات</label>
@@ -49,7 +56,9 @@ const AddPost = () => {
          </select>
          <label htmlFor="images">عکس</label>
          <input type="file" name="images" id="images" />
-         <button type='submit'>ایجاد</button>
+         <button
+            className='px-6 py-1.5 transition-all hover:bg-primary-Hover'
+            type='submit'>ایجاد</button>
       </form>
    );
 };
