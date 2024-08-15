6442bf6b2ad211a1a2ff2f3d28d5349dbbb309e0
Hamed Noroozi <noruzi206@gmail.com>
2024-08-15 20:49:54 +0330

    add styles of PostList

--- Frontend/src/components/templates/PostList.jsx
+++ Frontend/src/components/templates/PostList.jsx
@@ -3,26 +3,28 @@ import React from 'react';
 
 import Loader from 'components/modules/Loader';
 import { getPost } from 'services/user';
+import { sp } from 'utils/numbers';
 
 function PostList() {
    const baseURL = import.meta.env.VITE_BASE_URL;
    const { data, isLoading } = useQuery(["my-post-list"], getPost);
-   console.log(data?.data.posts);
 
    return (
       <div>
          <h3 className='my-8 border-b-2 border-primary w-fit pb-1 font-semibold'>آگهی‌های من</h3>
          {isLoading ? <Loader /> :
             (data ? data.data.posts.map(post => (
-               <div key={post._id}>
-                  <img src={`${baseURL}${post.images[0]}`} />
+               <div 
+               className='flex items-center border border-zinc-400 rounded my-2 p-1 [&_div]:w-full [&_p]:text-sm [&_span]:text-sm [&_span]:text-center'
+               key={post._id}>
+                  <img src={`${baseURL}${post.images[0]}`} className='w-full h-16 rounded ml-8' />
                   <div>
                      <p>{post.options.title}</p>
                      <span>{post.options.content}</span>
                   </div>
                   <div>
-                     <p>{post.createdAt}</p>
-                     <span>{post.amount} تومان</span>
+                     <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
+                     <span className='w-36 text-center'>{sp(post.amount)} تومان</span>
                   </div>
                </div>)) :
                (<div>
