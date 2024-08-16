353c9e4e5ab5c494ad868e9dc1c1ffad864a318b
Hamed Noroozi <noruzi206@gmail.com>
2024-08-16 21:40:11 +0330

    add sideBar and Main to HomePage

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-function HomePage(props) {
+import Main from 'components/templates/Main';
+import SideBar from 'components/templates/SideBar';
+import Loader from 'components/modules/Loader';
+
+import { getAllPost } from 'services/user';
+import { getCategory } from 'services/admin';
+
+function HomePage() {
+   const { data: posts, isLoading: postLoading } = useQuery(["my-post-list"], getAllPost);
+   const { data: categories, isLoading: categoryLoading } = useQuery(["get-categories"], getCategory);
+
    return (
-      <div>
-         HomePage
-      </div>
+      <>
+         {postLoading || categoryLoading ? <Loader /> :
+            <div className='flex flex-col items-center sm:flex-row sm:items-start'>
+               <SideBar categories={categories} />
+               <Main posts={posts} />
+            </div>}
+      </>
    );
 }
 
