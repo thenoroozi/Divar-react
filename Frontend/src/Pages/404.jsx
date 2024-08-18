e7a62deb8029556c21774e9a3a03ebc514e6d084
Hamed Noroozi <noruzi206@gmail.com>
2024-08-18 18:32:44 +0330

    add styles of 404 not found page

--- Frontend/src/Pages/404.jsx
+++ Frontend/src/Pages/404.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function PageNotFound(props) {
+function PageNotFound() {
    return (
-      <div>
-         PageNotFound
+      <div className='w-80 sm:w-96 text-center mx-auto mt-20 py-12 rounded-lg border-4 border-primary-Hover'>
+         <h3 className='sm:text-lg font-semibold'>ارور 404! پیج مورد نظر یافت نشد!</h3>
+         <button className='py-1.5 px-2 mt-3 text-sm sm:text-base transition-all hover:bg-primary-Hover'>
+            <Link to="/">برگشت به صفحه اصلی</Link>
+         </button>
       </div>
    );
 }
