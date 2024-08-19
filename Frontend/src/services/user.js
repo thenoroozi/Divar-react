d905acb1dfdc758d6be34ead33d9506278411dd7
Hamed Noroozi <noruzi206@gmail.com>
2024-08-19 22:12:15 +0330

    getting post details from api

--- Frontend/src/services/user.js
+++ Frontend/src/services/user.js
@@ -5,8 +5,10 @@ const getProfile = () => api.get("user/whoami")
 
 const getPost = () => api.get("post/my")
 
+const getPostDetails = ({queryKey}) => api.get(`post/${queryKey[1]}`)
+
 const getAllPost = () => api.get("")
 
 const deletePost = id => api.delete(`post/delete/${id}`)
 
-export { getProfile, getPost, deletePost, getAllPost };
+export { getProfile, getPost, deletePost, getAllPost, getPostDetails };
